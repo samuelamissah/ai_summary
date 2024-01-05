7f0d631c5cb9de86eb099e7228f58cbe820d757e
samuelamissah <skamissah@ug.edu.gh>
2024-01-05 10:45:31 +0000

    round 2

---
+++ src/components/Demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Demo = () => {
+  return (
+    <div>
+
+        
+    </div>
+  )
+}
+
+export default Demo
---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-4">
+        <img src={logo} alt="logo" className="w-32 object-contain" />
+        <button
+          type="button"
+          className="black_btn"
+          onClick={() => window.open("https://github.com/samuelamissah")}
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summerize Articles and Texts with <br className="max-md:hidden"/>
+        <span className=" orange_gradient"> Kojo and OPEN AI GPT-3 </span>
+        </h1>
+        <h2 className="sub_text desc">
+            Kojo is a tool that uses the power of GPT-3 to summarize articles and
+            texts. It is built with React, Tailwind CSS, and OpenAI API.
+        </h2>
+    </header>
+  );
+};
+
+export default Hero;
